56558baf1402b757e91e934ed9f10c4732af7fc4
Jessica Goulding <jgoulding2@gmail.com>
2014-07-01 17:32:06 -0600

    fixed typo in sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,5 +1,15 @@
 require 'csv'
-require_relative './merchant_repository'
+require_relative 'merchant_repository'
+require_relative 'merchant'
+require_relative 'customer_repository'
+require_relative 'customer'
+require_relative 'invoice_repository'
+require_relative 'invoice'
+require_relative 'item_repository'
+require_relative 'item'
+require_relative 'invoice_item_repository'
+require_relative 'invoice_item'
+# require_relative './transactions_repository'
 
 class SalesEngine
   attr_reader :customer_repository,
@@ -7,8 +17,8 @@ class SalesEngine
               :customer_repository,
               :invoice_repository,
               :invoice_item_repository,
-              :item_repository,
-              :transaction_repository
+              :item_repository
+              # :transaction_repository
 
   def startup
     @merchant_repository      = MerchantRepository.new
@@ -16,7 +26,9 @@ class SalesEngine
     @invoice_repository       = InvoiceRepository.new
     @invoice_item_repository  = InvoiceItemRepository.new
     @item_repository          = ItemRepository.new
-    @transaction_repository   = TransactionRepository.new
+
+    # @transaction_repository   = TransactionRepository.new
   end
+
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,22 +1,20 @@
 require 'csv'
 require_relative 'test_helper'
+require_relative '../lib/customer_repository'
+require_relative '../lib/invoice_repository'
+require_relative '../lib/item_repository'
+require_relative '../lib/invoice_item_repository'
+require_relative '../lib/merchant_repository'
+# require_relative '../lib/transactions_repository'
 require_relative '../lib/sales_engine'
 require 'pry'
 
 class SalesEngineTest < Minitest::Test
+  attr_reader :engine
+
   def setup
     @engine ||= SalesEngine.new
   end
-
-  def test_it_exists
-    assert engine
-  end
-
-  # def test_it_loads_a_file
-  #   engine = SalesEngine.new
-  #   filename = "./test/fixtures/merchants.csv"
-  #   assert engine.startup(filename)
-  # end
 
   def test_it_loads_the_files
     assert engine.startup
@@ -26,9 +24,16 @@ class SalesEngineTest < Minitest::Test
     engine.startup
     assert engine.merchant_repository
     assert engine.invoice_repository
-    # assert engine.item_repository
-    # assert engine.invoice_item_repository
-    # assert engine.customer_repository
+    assert engine.item_repository
+    assert engine.invoice_item_repository
+    assert engine.customer_repository
     # assert engine.transactions_repository
   end
+
+  # def test_it_loads_a_file
+  #   engine = SalesEngine.new
+  #   filename = "./test/fixtures/merchants.csv"
+  #   assert engine.startup(filename)
+  # end
+
 end
