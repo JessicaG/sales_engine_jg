7b15dd0a41cec95c0b274594690639f3f418e44d
Jessica Goulding <jgoulding2@gmail.com>
2014-07-05 16:11:19 -0600

    started working through testing for BI and most revenue

--- test/merchant_relationships_test.rb
+++ test/merchant_relationships_test.rb
@@ -20,6 +20,28 @@ class MerchantRelationshipsTest < Minitest::Test
     assert ['Item Qui Esse'], items.collect.count(&:name)
   end
 
+  #most_revenue(x) returns the top x merchant instances ranked by total revenue
+  # we need to take each merchant and find out how much revnue they generated
+  # and return the merchant with the most revenue
+
+  # how do we find revenue by merchant??
+  # items has a unit price and merchant_id
+  # invoice_items have a invoice_id and a quantity
+
+  # the paremeter is the number of merchants they want returned, who are the top sellers
+
+  def test_it_can_return_an_array_of_merchants_with_revenue
+    # someway we need to loop through the merchants, and find the revenue for each one
+    # merchant = engine.invoice_repository.find_by('merchant_id', '38')
+    # invoices = engine.invoice_item_repository.find_by('invoice_id', '')
+
+    #group_by[array_of_merchant_ids_in_our_invoices]
+    
+
+    assert # thismerchant.name, most_revenue(1)
+  end
+
+
   # def test_items_has_the_correct_number_of_them
   #  merchant = engine.merchant_repository.find_by_name ('Kirlin, Jakubowski and Smitham')
   #  items = merchant.items
